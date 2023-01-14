0c932cdc0fa860625b5fca03e728723b2adaab67
I-3 <pytaia@yandex.ru>
2023-01-14 18:46:40 +0300

    Clean file with functions and class

--- labirint generation.py
+++ labirint generation.py
@@ -1,7 +1,7 @@
-from random import choice, randrange
+from random import choice
 
 
-abscissa_dimension, ordinate_dimension = list(map(int, input().split()))
+abscissa_dimension, ordinate_dimension, labyrinth_field = 0, 0, []
 
 
 class Cell:
@@ -36,39 +36,28 @@ class Cell:
         return int(''.join(map(str, self.walls)), 2)
 
 
+def labyrinth_recoding():
+    return [[cell.recoding() for cell in labyrinth_field[i * abscissa_dimension:(i + 1) * abscissa_dimension]]
+            for i in range(ordinate_dimension)]
+
+
+def transferring_variables(abscissa, ordinate, labyrinth):
+    global abscissa_dimension, ordinate_dimension, labyrinth_field
+    abscissa_dimension, ordinate_dimension, labyrinth_field = abscissa, ordinate, labyrinth
+
+
 def remove_walls(current, next):
-    dx = current.x - next.x
-    if dx == 1:
+    delta_x = current.x - next.x
+    if delta_x == 1:
         current.walls[3] = 0
         next.walls[1] = 0
-    elif dx == -1:
+    elif delta_x == -1:
         current.walls[1] = 0
         next.walls[3] = 0
-    dy = current.y - next.y
-    if dy == 1:
+    delta_y = current.y - next.y
+    if delta_y == 1:
         current.walls[0] = 0
         next.walls[2] = 0
-    elif dy == -1:
+    elif delta_y == -1:
         current.walls[2] = 0
         next.walls[0] = 0
-
-
-labyrinth_field = [Cell(col, row) for row in range(ordinate_dimension) for col in range(abscissa_dimension)]
-current_cell = labyrinth_field[randrange(abscissa_dimension * ordinate_dimension)]
-stack = []
-stack_count = 1
-
-while ordinate_dimension * abscissa_dimension != stack_count:
-    current_cell.visited = True
-    next_cell = current_cell.check_neighbors()
-    if next_cell:
-        next_cell.visited = True
-        stack.append(current_cell)
-        stack_count += 1
-        remove_walls(current_cell, next_cell)
-        current_cell = next_cell
-    elif stack:
-        current_cell = stack.pop()
-
-print([[cell.recoding() for cell in labyrinth_field[i * abscissa_dimension:(i + 1) * abscissa_dimension]]
-       for i in range(ordinate_dimension)])
